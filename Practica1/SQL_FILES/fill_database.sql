f752ab615fdd5d80ce28a6e7cf3b8a324cebafa4
Carlos Campos <carloscamposprivated@gmail.com>
2021-12-19 04:10:04 -0600

    Fixing - Script carga a modelo

--- Practica1/SQL_FILES/fill_database.sql
+++ Practica1/SQL_FILES/fill_database.sql
@@ -24,7 +24,14 @@ FROM geonames_temp;
 INSERT IGNORE INTO project(id_project,id_geocoded,project_title, start_actual_isodate, end_actual_isodate, donors, donors_iso3,recipients,ad_sector_codes, ad_sector_names,status,transactions_start_year, transactions_end_year,total_commitments,total_disbursements)
 SELECT pt.project_id, pt.is_geocoded, pt.project_title, pt.start_actual_isodate, pt.end_actual_isodate,pt.donors, pt.donors_iso3,cc.id,pt.ad_sector_codes,pt.ad_sector_names,s.id,pt.transactions_start_year,pt.transactions_end_year,CAST(pt.total_commitments AS DECIMAL(19,5)),CAST(pt.total_disbursements AS DECIMAL(19,5))
 FROM projects_temp as pt, status as s, countrycodes as cc
+WHERE pt.recipients = cc.name AND pt.status = s.name;
 
 INSERT INTO transaction(id_transaction,id_project,transaction_isodate,transaction_year,transaction_value_code, transaction_currency,transaction_value)
 SELECT tt.transaction_id, p.id,  STR_TO_DATE(tt.transaction_isodate,"%d/%m/%Y"), tt.transaction_year, tt.transaction_value_code, c.id, tt.transaction_value
 FROM transactions_temp as tt, project as p, currency as c
+WHERE tt.project_id = p.id_project AND tt.transaction_currency = c.name_currency;
+
+INSERT IGNORE INTO level_1a(id_project,id_project_location,id_geoname, transaction_start_year, transaction_end_year, even_split_commitments, even_split_disbursements)
+SELECT DISTINCT p.id, l.project_location_id, g.id, l.transactions_start_year, l.transactions_end_year, CAST(l.even_split_commitments AS DECIMAL(19,5)),  CAST(l.even_split_disbursements AS DECIMAL(19,5))
+FROM level_1a_temp as l, project as p, geoname as g
+WHERE l.project_id = p.id_project AND l.geoname_id = g.id_geoname;
